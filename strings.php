5b5854230011dcb41776d21c3fbc236cb19d7f13
Farhan Farooq <faarhaanfaarooq@gmail.com>
2024-02-07 22:34:07 +0600

    .

--- strings.php
+++ strings.php
@@ -1,5 +1,7 @@
 <?php
 $str = "The";
 $strlen = strlen($str);
-echo "The length of the string is: ".$strlen;
+echo "The length of the string is: ".$strlen."Thank You!";
+$greetings = "Hello, Farhan! How are you?";
+echo "<br>Total words in greetings: ".str_word_count($greetings);
 ?>
